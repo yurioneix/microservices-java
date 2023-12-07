fe03e2096310bafafb81524beafcd8943c26404a
yurioneix <yurioneix18@gmail.com>
2023-12-07 12:31:40 -0300

    feat: cria método para publicação de mensagens na fila na classe UserProducer

--- User/src/main/java/com/microservices/User/producers/UserProducer.java
+++ User/src/main/java/com/microservices/User/producers/UserProducer.java
@@ -1,5 +1,7 @@
 package com.microservices.User.producers;
 
+import com.microservices.User.dtos.EmailDto;
+import com.microservices.User.models.UserModel;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -14,4 +16,14 @@ public class UserProducer {
 
   @Value(value = "${broker.queue.email.name}")
   private String routingKey;
+
+  public void publishEmailMessage(UserModel userModel) {
+    EmailDto newEmail = new EmailDto();
+    newEmail.setUserId(userModel.getUserID());
+    newEmail.setEmailTo(userModel.getEmail());
+    newEmail.setSubject("Cadastro realizado com sucesso!");
+    newEmail.setText(userModel.getName() + "seja bem vindo(a)! \n Agradecemos o seu cadastro, aproveite agora todos os recursos da nossa plataforma!");
+
+    rabbitTemplate.convertAndSend("", routingKey, newEmail);
+  }
 }
