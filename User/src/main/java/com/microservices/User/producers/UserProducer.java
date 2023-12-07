dbabfccb4b3f1b1b0501261210d3cdbdba8a1153
yurioneix <yurioneix18@gmail.com>
2023-12-07 12:21:56 -0300

    feat: cria pacote producers e declara a classe UserProducer com seus atributos

---
+++ User/src/main/java/com/microservices/User/producers/UserProducer.java
@@ -0,0 +1,17 @@
+package com.microservices.User.producers;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserProducer {
+  final RabbitTemplate rabbitTemplate;
+
+  public UserProducer(RabbitTemplate rabbitTemplate) {
+    this.rabbitTemplate = rabbitTemplate;
+  }
+
+  @Value(value = "${broker.queue.email.name}")
+  private String routingKey;
+}
